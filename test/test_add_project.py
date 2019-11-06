f344668fb401c3788c626d4506c3a13ce0c6574e
Sergey <dessummiis@users.noreply.github.com>
2019-11-06 10:13:07 +0300

    json_projects

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,6 +1,7 @@
 
 
-def test_add_project(app):
+def test_add_project(app, json_projects):
+    project = json_projects
     # Проверить есть ли созданные группы
     # Создать группу если нет групп
     app.project.create(project)
