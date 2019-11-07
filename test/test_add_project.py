13878ac4243f3fb499b90edec3d00e4a4fedeb8e
Sergey <dessummiis@users.noreply.github.com>
2019-11-07 10:18:06 +0300

    wip

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,6 +1,8 @@
 
 
 def test_add_project(app, json_projects):
+    app.session.login("administrator", "root")
+    assert app.session.is_logged_in_as("administrator")
     project = json_projects
     # Проверить есть ли созданные группы
     # Создать группу если нет групп
