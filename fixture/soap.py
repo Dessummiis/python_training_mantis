c889e3a5a016296a539eeabfdf47fca735ee425a
Sergey <s.dobrovolskiy@rtk-dc.ru>
2019-11-15 17:04:37 +0300

    26 WIP

--- fixture/soap.py
+++ fixture/soap.py
@@ -17,11 +17,10 @@ class SoapHelper:
 
     def get_project_list(self, username, password):
         client = self.wsdl()
-        try:
-            client.service.mc_projects_get_user_accessible(username, password)
-            return True
-        except WebFault:
-            return False
+        project_list = []
+        project_list = client.service.mc_projects_get_user_accessible(username, password)
+        return project_list
+
 
     def wsdl(self):
         client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -9,13 +9,13 @@ def random_string(maxlen):
 
 
 def test_add_project(app):
+    username = app.config['webadmin']['username']
+    password = app.config['webadmin']['password']
     project = Project(name=random_string(10), description=random_string(20))
-    # app.session.login("administrator", "root")
-    # assert app.session.is_logged_in_as("administrator")
     # old_projects = app.project.get_project_list()
-    old_projects = app.soap.get_project_list()
+    old_projects = app.soap.get_project_list(username, password)
     app.project.create(project)
     # new_projects = app.project.get_project_list()
-    new_projects = app.soap.get_project_list()
+    new_projects = app.soap.get_project_list(username, password)
     old_projects.append(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
